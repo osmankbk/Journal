e2ef5587d562a32238245d9f22a0a5808d0242dc
osmankbk <mackiekbk@yahoo.com>
2022-03-15 20:47:39 -0400

    Changes about-summary <p> width from 13% to 50%

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,8 +5,8 @@ import linkedin from '../linkedin.png';
 // My About page component
 const About = () => {
     return(
-        <div className="conatct-main-div container-fluid about bg-light text-light">
-            <div className="container-fluid about-two">
+        <div className="conatct-main-div about bg-light text-light">
+            <div className="about-two">
                 <h1 className="about-header">About</h1>
                 <p className="about-summary">Dotπdown is a web app that makes journaling easy and efficient entry, editing, 
                     updating, deletion, and always accessible wherever and whenever. It’s also a space of meditation; dotting down 
